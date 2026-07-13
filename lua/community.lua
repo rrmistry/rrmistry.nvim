bd623664168fffb363bd65dc71c568a3472940be
Rohit Mistry <rohit.mistry@righteous.ai>
2026-07-13 12:33:00 -0400

    Added noice-nvim

--- lua/community.lua
+++ lua/community.lua
@@ -30,4 +30,5 @@ return {
   { import = "astrocommunity.test.neotest" }, -- pytest runner (<Space>T*), adapter from pack.python
   { import = "astrocommunity.code-runner.overseer-nvim" }, -- task runner, reads .vscode/tasks.json (<Space>M*)
   { import = "astrocommunity.motion.nvim-surround" }, -- ys/cs/ds surround operators
+  { import = "astrocommunity.utility.noice-nvim" },
 }
